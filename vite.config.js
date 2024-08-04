57a611d9980a691305ec56b3451f5031f4d7016e
Pablo Lambert <pablomartin.lambert@gmail.com>
2024-08-03 20:52:38 -0400

    prueba deploy2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hito-1-pizzeria-mamma-mia/"
+  base: "/hito-1-pizzeria-mamma-mia/",
 })
