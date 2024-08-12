b9028080cfc8415ea3d50ba698e2a02a4cdd13ed
Pablo Lambert <pablomartin.lambert@gmail.com>
2024-08-12 00:44:24 -0400

    Hito 2 estados y componentes para formulario de registro y login

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Footer from './components/Footer'
+import Registro from './components/Registro'
+import Login from './components/Login'
+
 
 function App() {
   return (
     <>
       <Navbar/>
-      <Home/>
+      {/* <Home/> */}
+      <Registro />
+      <Login />
       <Footer/>
     </>
   )
