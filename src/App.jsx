78d04060eee9119478a93469706a4572180ead07
Pablo Lambert <pablomartin.lambert@gmail.com>
2024-08-13 23:25:56 -0400

    prueba pull

--- src/App.jsx
+++ src/App.jsx
@@ -10,28 +10,18 @@ import Home from './components/Home'
 import Footer from './components/Footer'
 import Registro from './components/Registro'
 import Login from './components/Login'
-<<<<<<< HEAD
 import Cart from './components/Cart'
 //Importando data de pizzas
 //import { pizzas } from './data/pizzas'
-=======
-
->>>>>>> ad69b6079c7d36eecec48e8805ecd380c8d431a7
 
 function App() {
   return (
     <>
       <Navbar/>
-<<<<<<< HEAD
       <Home/>
       <Registro/>
       <Login/>
       {/* <Cart/> */}
-=======
-      {/* <Home/> */}
-      <Registro />
-      <Login />
->>>>>>> ad69b6079c7d36eecec48e8805ecd380c8d431a7
       <Footer/>
     </>
   )
